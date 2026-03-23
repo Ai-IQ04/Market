977284057b0d3f45a60cf00a8bd602462ed13ff9
Anusat07 <limpirat@gmail.com>
2026-03-23 20:43:32 +0700

    chore: update styles and meta viewport

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -21,6 +21,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body className={`${inter.variable} ${prompt.variable}`}>
         {children}
       </body>
